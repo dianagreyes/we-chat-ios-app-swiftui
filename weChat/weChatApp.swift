d977eb4bc077f508357d140af805cce97ea48478
dianagreyes <dianagreyes29@gmail.com>
2024-07-11 22:43:59 -0600

    Initial Commit

---
+++ weChat/weChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  weChatApp.swift
+//  weChat
+//
+//  Created by Giselle RS on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct weChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
